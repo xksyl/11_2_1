a844363eded18b6a4a49cbd2dc0fe3ad22176144
xksyl <egorka2004212@gmail.com>
2024-10-15 16:41:36 +0500

    Update module 11_2.py

--- module 11_2.py
+++ module 11_2.py
@@ -5,8 +5,9 @@ def introspection_info(obj):
     info = {}
 
     info['type'] = type(obj).__name__
-    info['attributes'] = dir(obj)
+    info['attributes and methods'] = dir(obj)
     info['methods'] = [method for method in dir(obj) if callable(getattr(obj, method))]
+    info ['methods'] = [attr for attr in dir(obj) if not callable(getattr(obj, attr))]
     module = inspect.getmodule(obj)
     info['module'] = module.__name__ if module else 'builtins'
 
